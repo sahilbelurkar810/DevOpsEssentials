41be0c522b428425797228dac52c0d899d66d980
sahilbelurkar810 <sahilbelurkar28@gmail.com>
2024-11-27 15:19:54 +0530

    Addeed XOR swap

--- python/operators.py
+++ python/operators.py
@@ -50,6 +50,10 @@ print(f"\nAfter swapping var1:{var1} and var2:{var2}")
 
 
 print("using xor")
+var1 = var1 ^ var2
+var2 = var1 ^ var2
+var1 = var1 ^ var2
+print(f"\nAfter swapping var1:{var1} and var2:{var2}")
 
 # for i in range(1,11):
 #     print(f"{num1} * {i} = {num1*i}")
